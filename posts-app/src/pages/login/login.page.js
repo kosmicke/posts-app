4a2ac3ccf30e87885b11827079e4a0f1d5afa6fe
André Rodrigues <andre.urbanico@gmail.com>
2020-09-30 23:04:20 -0300

    implemented hooks

--- posts-app/src/pages/login/login.page.js
+++ posts-app/src/pages/login/login.page.js
@@ -6,6 +6,7 @@ class Login extends React.Component{
 
     constructor(props){
         super(props)
+        
         this.state = {
             userName : "",
             password : "",
--- posts-app/src/pages/post-detail/post-detail.component.js
+++ posts-app/src/pages/post-detail/post-detail.component.js
@@ -1,107 +1,105 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 import PageTop from '../../components/page-top/page-top.component';
 import authService from '../../services/auth.service';
 import postsService from '../../services/posts.service';
 import './post-detail.component.css'
 
-class PostDetail extends React.Component {
+const PostDetail = ({ history }) => {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            post: null,
-            redirectTo: null
+    let [post, setPost] = useState(null);
+    let [redirectTo, setRedirectTo] = useState(null);
+    let {id} = useParams()
+
+    useEffect(() => {
+
+        const loadPost = async (postId) => {
+            try {
+                let res = await postsService.getOne(postId)
+                setPost(res.data.data[0])
+            } catch (error) {
+                console.log(error);
+                alert("Não foi possível carregar post.")
+            }
         }
-    }
 
-    componentDidMount() {
         let userData = authService.getLoggedUser();
         if (!userData) {
-            this.setState({ redirectTo: "/login" })
+            setRedirectTo("/login")
         } else {
-            let postId = this.props.match.params.id
-            this.loadPost(postId)
+            loadPost(id)
         }
-    }
-
-    async loadPost(postId) {
-        try {
-            let res = await postsService.getOne(postId)
-            this.setState({ post: res.data.data[0] })
-        } catch (error) {
-            console.log(error);
-            alert("Não foi possível carregar post.")
-        }
-    }
-
-    async deletePost(postId) {
         
+    }, [])
+
+    const deletePost = async (postId) => {
+
         if (!window.confirm("Deseja realmente excluir este post?")) return;
 
         try {
             await postsService.delete(postId)
             alert("Post excluído com sucesso")
-            this.props.history.replace('/post-list')
+            history.replace('/post-list')
         } catch (error) {
             console.log(error);
             alert("Não foi excluir o post.")
         }
+
     }
 
-    render() {
-        if (this.state.redirectTo) {
-            return (
-                <Redirect to={this.state.redirectTo} />
-            )
-        }
+    if (redirectTo) {
         return (
-            <div className="container">
-
-                <PageTop title='Post' desc='Detalhes do post'>
-                    <button className="btn btn-light" onClick={() => this.props.history.goBack()}>
-                        Voltar
-                    </button>
-                </PageTop>
-
-                <div className="row">
-                    <div className="col-6">
-                        <img className="post-img" src={this.state?.post?.imageUrl} alt="image" />
-                    </div>
-                    <div className="col-6">
-                        <div className="post-info">
-                            <h4>ID</h4>
-                            <p>{this.state.post?.id}</p>
-                        </div>
-                        <div className="post-info">
-                            <h4>Título</h4>
-                            <p>{this.state.post?.title}</p>
-                        </div>
-                        <div className="post-info">
-                            <h4>Conteúdo</h4>
-                            <p>{this.state.post?.content}</p>
-                        </div>
-                        <div className="btn-group" role="group" aria-label="Basic example">
-                            <button
-                                type="button"
-                                className="btn btn-sm btn-outline-danger"
-                                onClick={() => this.deletePost(this.state.post.id)}>
-                                Excluir
-                            </button>
-                            <button
-                                type="button"
-                                className="btn btn-sm btn-outline-primary"
-                                onClick={() => this.props.history.push('/post-edit/' + this.state.post.id)}>
-                                Editar
-                            </button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+            <Redirect to={redirectTo} />
         )
     }
 
+    return (
+        <div className="container">
+
+            <button onClick={() => setPost(Math.random())}> Atualizar</button>
+
+            <PageTop title='Post' desc='Detalhes do post'>
+                <button className="btn btn-light" onClick={() => history.goBack()}>
+                    Voltar
+                </button>
+            </PageTop>
+
+            <div className="row">
+                <div className="col-6">
+                    <img className="post-img" src={post?.imageUrl} alt="image" />
+                </div>
+                <div className="col-6">
+                    <div className="post-info">
+                        <h4>ID</h4>
+                        <p>{post?.id}</p>
+                    </div>
+                    <div className="post-info">
+                        <h4>Título</h4>
+                        <p>{post?.title}</p>
+                    </div>
+                    <div className="post-info">
+                        <h4>Conteúdo</h4>
+                        <p>{post?.content}</p>
+                    </div>
+                    <div className="btn-group" role="group" aria-label="Basic example">
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-outline-danger"
+                            onClick={() => deletePost(post.id)}>
+                            Excluir
+                            </button>
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-outline-primary"
+                            onClick={() => history.push('/post-edit/' + post.id)}>
+                            Editar
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
-export default PostDetail
+export default PostDetail;
